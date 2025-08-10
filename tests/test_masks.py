10f1384c9c05f550895aeaf00a902dcc3c58f51a
Vadim Nadrin <younglapes1337@gmail.com>
2025-08-10 19:36:44 +0700

    test_masks corrected

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,4 +1,5 @@
 import pytest
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
@@ -10,8 +11,9 @@ from src.masks import get_mask_account, get_mask_card_number
                                                    ("1337666777228282", "1337 66** **** 8282"),
                                                    ("89909221136652292", "Несуществующий номер карты"),
                                                    ("5e99941f228426c3", "Несуществующий номер карты")])
-def test_get_mask_card(card_number, expected):
+def test_get_mask_card(card_number: str, expected: str) -> str:
     assert get_mask_card_number(card_number) == expected
+    return expected
 
 
 @pytest.mark.parametrize("account_number, expected", [("73654108430135874305", "**4305"),
@@ -21,5 +23,6 @@ def test_get_mask_card(card_number, expected):
                                                       ("11111111111111111111", "**1111"),
                                                       ("646864736788947795890", "Неверный номер аккаунта"),
                                                       ("0z0f00h00g00r00e00c0", "Неверный номер аккаунта")])
-def test_get_mask_account(account_number, expected):
+def test_get_mask_account(account_number: str, expected: str) -> str:
     assert get_mask_account(account_number) == expected
+    return expected
