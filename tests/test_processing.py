bf2af08684ae0238072c92a18782430a49d9eb65
Vadim Nadrin <younglapes1337@gmail.com>
2025-08-10 19:36:53 +0700

    test_processing corrected

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,12 +1,17 @@
 import pytest
+
 from src.processing import filter_by_state, sort_by_date
 
 
-def test_filter_by_state(dict_list, state_executed, state_canceled):
+def test_filter_by_state(dict_list: list, state_executed: list, state_canceled: list) -> list:
     assert filter_by_state(dict_list) == state_executed
+    return state_executed
     assert filter_by_state(dict_list, state="CANCELED") == state_canceled
+    return state_canceled()
 
 
-def test_sort_by_date(dict_list, sort_true, sort_false):
+def test_sort_by_date(dict_list: list, sort_true: list, sort_false: list) -> list:
     assert sort_by_date(dict_list) == sort_true
+    return sort_true
     assert sort_by_date(dict_list, descending=False) == sort_false
+    return sort_false
