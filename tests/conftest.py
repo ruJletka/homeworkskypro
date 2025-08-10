45826f90f48d49c1259428a5c0d3af720a7615fb
Vadim Nadrin <younglapes1337@gmail.com>
2025-08-10 19:36:37 +0700

    conftest corrected

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.fixture
-def dict_list():
+def dict_list() -> list:
     return ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
              {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
@@ -10,19 +10,19 @@ def dict_list():
 
 
 @pytest.fixture
-def state_canceled():
+def state_canceled() -> list:
     return ([{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
 
 
 @pytest.fixture
-def state_executed():
+def state_executed() -> list:
     return ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
              {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}])
 
 
 @pytest.fixture
-def sort_true():
+def sort_true() -> list:
     return ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
@@ -30,7 +30,7 @@ def sort_true():
 
 
 @pytest.fixture
-def sort_false():
+def sort_false() -> list:
     return ([{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
