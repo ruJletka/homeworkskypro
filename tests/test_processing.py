94858813336ce4c7bbf18708a8ffce0d44aa3c46
Vadim Nadrin <younglapes1337@gmail.com>
2025-08-10 18:48:05 +0700

    add test_processing.py

---
+++ tests/test_processing.py
@@ -0,0 +1,14 @@
+from pickle import FALSE
+
+import pytest
+from src.processing import filter_by_state, sort_by_date
+
+
+def test_filter_by_state(dict_list, state_executed, state_canceled):
+    assert filter_by_state(dict_list) == state_executed
+    assert filter_by_state(dict_list, state="CANCELED") == state_canceled
+
+
+def test_sort_by_date(dict_list, sort_true, sort_false):
+    assert sort_by_date(dict_list) == sort_true
+    assert sort_by_date(dict_list, descending=False) == sort_false
