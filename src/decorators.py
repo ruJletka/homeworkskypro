3e1dd32d1c6fff369338c79b2868c0263caafd51
Vadim Nadrin <younglapes1337@gmail.com>
2025-08-17 08:19:25 +0700

    add decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,30 @@
+from functools import wraps
+
+
+def log(filename = None):
+    """Декоратор, который будет автоматически логировать начало и конец выполнения функции,
+     а также ее результаты или возникшие ошибки."""
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            try:
+                result = function(*args, **kwargs)
+                name_function = function.__name__
+                if filename:
+                    file = open(filename, "a", encoding="utf-8")
+                    file.write(f"Функция {name_function} ок. Результат: {result}" + "\n")
+                    file.close()
+                else:
+                    print(f"{name_function} ок. Результат: {function(*args, **kwargs)}")
+            except Exception as e:
+                result = None
+                print(f"{function.__name__} error: {e}. Inputs: {args}, {kwargs}")
+            except ZeroDivisionError:
+                result = None
+                print(f"{function.__name__} error: ZeroDivisionError. Inputs: {args}, {kwargs}")
+
+            return result
+
+        return wrapper
+
+    return decorator
