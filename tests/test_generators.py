3915742808aec66df53f258ba2bce4f7561b6ea1
Vadim Nadrin <younglapes1337@gmail.com>
2025-08-17 07:53:31 +0700

    fixes in test_generators.py

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -5,13 +5,9 @@ from src.generators import filter_by_currency, transaction_descriptions, card_nu
 
 def test_filter_by_currency(trans_list: list, usd_curr: list, rub_curr: list, eur_curr: list) -> list:
     assert [filter_by_currency(trans_list, "USD") == usd_curr]
-    return usd_curr
     assert [filter_by_currency(trans_list, "RUB") == rub_curr]
-    return rub_curr
     assert [filter_by_currency([], "USD") == []]
-    return []
     assert [filter_by_currency(trans_list, "EUR") == eur_curr]
-    return eur_curr
 
 
 @pytest.mark.parametrize("input_data, expected_output", [([{"description": "Оплата товара"}], ["Оплата товара"]),
