fe9c8d4901d6e4772bfcaf090c9b0403f82fca72
Vadim Nadrin <younglapes1337@gmail.com>
2025-08-18 16:28:34 +0700

    test_decorators fixes

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,3 +1,5 @@
+from fileinput import filename
+
 import pytest
 
 from src.decorators import log
@@ -22,7 +24,7 @@ def test_log_error(capsys):
     result = divide(10, 0)
     captured = capsys.readouterr()
     assert result is None
-    assert "divide error: division by zero. Inputs: (10, 0)" in captured.out
+    assert "divide error: ZeroDivisionError. Inputs: (10, 0)" in captured.out
 
 
 def test_log_file(tmp_path):
@@ -36,3 +38,20 @@ def test_log_file(tmp_path):
 
     with open(log_file, "r", encoding="utf-8") as file:
         assert "Функция sum_numbers ок. Результат: 5" in file.read()
+
+
+def test_error_log_file(tmp_path):
+    log_file = tmp_path / "error_log.txt"
+
+    @log(filename=str(log_file))
+    def faulty_func():
+        raise ValueError("Test error")
+
+    faulty_func()
+
+    with open(log_file) as f:
+        content = f.read()
+
+    assert "faulty_func error: ValueError" in content
+    assert "Inputs: (), {}" in content
+    
