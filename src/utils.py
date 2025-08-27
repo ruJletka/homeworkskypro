d40b9e401c109be624e034d6e1ee27d5f102c7ef
Vadim Nadrin <younglapes1337@gmail.com>
2025-08-27 12:30:02 +0700

    rework function on utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,14 @@ import json
 import os
 
 
-def load_transaction_data(file_path: str) -> list[dict[str, any]]:
-    """Функция загружает данные о финансовых операциях из JSON-файла."""
+def load_transaction_data() -> list[dict[str, any]]:
+    """Функция загружает данные о финансовых операциях из файла operations.json в директории /data."""
     try:
+        file_path = os.path.join('data', 'operations.json')
+
         if not os.path.exists(file_path):
             return []
+
         if os.path.getsize(file_path) == 0:
             return []
 
